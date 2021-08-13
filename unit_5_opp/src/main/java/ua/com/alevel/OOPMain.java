baa4f9aa0c812304636ebf53ff37700d40a2d875
Denys-Bardinov <denys.bardinov@nixsolutions.com>
2021-08-13 21:22:39 +0300

    1

---
+++ unit_5_opp/src/main/java/ua/com/alevel/OOPMain.java
@@ -0,0 +1,9 @@
+package ua.com.alevel;
+
+import ua.com.alevel.controller.UserController;
+
+public class OOPMain {
+    public static void main(String[] args) {
+       new UserController().start();
+    }
+}

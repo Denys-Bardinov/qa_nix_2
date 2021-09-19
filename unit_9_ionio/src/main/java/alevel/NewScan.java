0880b390235293c4c43800419b567a1f5cddd273
Denys-Bardinov <denys.bardinov@nixsolutions.com>
2021-09-19 11:52:05 +0300

    1

--- unit_9_ionio/src/main/java/alevel/NewScan.java
+++ unit_9_ionio/src/main/java/alevel/NewScan.java
@@ -3,13 +3,5 @@ package alevel;
 import java.util.Scanner;
 
 public class NewScan {
-    String scanResult;
 
-    public String getScanResult() {
-        return scanResult;
-    }
-
-    public void setScanResult(String scanResult) {
-        this.scanResult = scanResult;
-    }
 }

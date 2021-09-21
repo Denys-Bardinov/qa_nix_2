91bbc235dbdb7d5fb3f244fc1c2099b68579b8b2
Denys-Bardinov <denys.bardinov@nixsolutions.com>
2021-09-21 22:34:53 +0300

    1

--- unit_9_ionio/src/main/java/alevel/Fwriter.java
+++ unit_9_ionio/src/main/java/alevel/Fwriter.java
@@ -1,9 +1,8 @@
 package alevel;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Fwriter {
@@ -11,24 +10,41 @@ public class Fwriter {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите предложение содержащее полиндромы и одинаковые слова:");
         String scannerResult = scanner.nextLine();
-        System.out.println("Посмотрите результат в text.txt");
+        System.out.println("Посмотрите результат в output.txt");
+        File myFile1 = new File("input.txt");
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(myFile1));
+            writer.write(scannerResult);
+            writer.flush();
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        String textInPath = "input.txt";
+        String value = null;
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(textInPath))) {
+            value = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         SimilarWords similarWords = new SimilarWords();
-        String b = similarWords.calculateSimilarWords(scannerResult);
+        String b = similarWords.calculateSimilarWords(value);
         String SimilarWordsResult = ("Одинаковый слова: " + "\n" + b);
 
         Sentences sentences = new Sentences();
-        int sentnecesCount = sentences.calculateSentences(scannerResult);
+        int sentnecesCount = sentences.calculateSentences(value);
         String sentenceCountString = Integer.toString(sentnecesCount);
         String sentnecesCountResult = ("Количество предложений = " + sentenceCountString);
 
         Polindrome polindrome = new Polindrome();
-        int polindromeCount = polindrome.calculatePolindrome(scannerResult);
+        int polindromeCount = polindrome.calculatePolindrome(value);
         String polindromeCountString = Integer.toString(polindromeCount);
         String polindromeCountResult = ("Количество полиндромов = " + polindromeCountString);
 
-        File myFile = new File("text.txt");
+        File myFile = new File("output.txt");
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(myFile));
             writer.write(polindromeCountResult + "\n" + sentnecesCountResult + "\n" + SimilarWordsResult);
